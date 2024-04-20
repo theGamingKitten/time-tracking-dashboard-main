2f247e514a822717eb342da6aacc39cb4d95588e
theGamingKitten <56452901+theGamingKitten@users.noreply.github.com>
2024-04-20 23:12:56 +0200

    version mobile

--- README.md
+++ README.md
@@ -1,8 +1,63 @@
-# React + Vite
+# Frontend Mentor - Time tracking dashboard solution
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This is a solution to the [Time tracking dashboard challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/time-tracking-dashboard-UIQ7167Jw). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
 
-Currently, two official plugins are available:
+## Table of contents
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshots](#screenshots)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Switch between viewing Daily, Weekly, and Monthly stats
+
+### Screenshots
+
+![](./screenshot.jpg)
+![](./screenshot_mobile.jpg)
+
+### Links
+
+- [Solution](https://your-solution-url.com)
+- [Live](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
+
+### What I learned
+
+-
+
+### Continued development
+
+-
+
+### Useful resources
+
+- [Example resource 1](https://www.example.com) - This helped me for XYZ reason. I really liked this pattern and will use it going forward.
+
+## Author
+
+- Frontend Mentor - [@theGamingKitten](https://www.frontendmentor.io/profile/theGamingKitten)
--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import imgSrc from './assets/image-jeremy.png';
+import dotsSvg from './assets/icon-ellipsis.svg';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
     <>
-      <div className="logos">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://tailwindcss.com/" target='_blank'>          
-        <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/320px-Tailwind_CSS_Logo.svg.png" />          
-        </a>
-      </div>
-      <h1>Vite + React + TailwindCSS</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the logos to learn more
-      </p>
+      <main className='grid grid-flow-row gap-5'>
+        <div id="hero-card"
+        className='w-full rounded-lg'>
+          <div 
+            className='bg-blue p-5 rounded-lg flex flex-row items-center justify-around'
+            id="hero-card-top">
+            <img 
+              src={imgSrc} 
+              className='w-16 rounded-full border border-white inline-block'
+            />
+            <span className='font-light'>
+              <span className='block text-sm'>Report for</span>
+              <span className='text-lg'>Jeremy Robson</span>
+            </span>
+          </div>
+          <div 
+            className='px-7 py-5 text-sm text-desaturated-blue bg-dark-blue flex flex-row items-center justify-between'
+            id="hero-card-bottom">
+            <span>Daily</span>
+            <span>Weekly</span>
+            <span>Monthly</span>
+          </div>
+        </div>
+        <div id="work-card"
+          className='pt-8 bg-work bg-no-repeat bg-right-top bg-light-red-work flex flex-col justify-end rounded-lg h-13'>                          
+            <div id="work-card-bottom"
+              className='bg-dark-blue rounded-lg grid grid-cols-2 grid-rows-2 p-6 gap-x-8 items-center justify-between'>
+                <span className='font-normal'>Work</span>
+                <img className='justify-self-end' src={dotsSvg}/>
+                <span className='text-3xl font-light'>32hrs</span>
+                <span className='text-pale-blue text-xs justify-self-end'>Last Week - 36hrs</span>
+            </div>
+        </div>
+        <div id="play-card"
+          className='pt-8 bg-play bg-no-repeat bg-right-top bg-soft-blue-play flex flex-col justify-end rounded-lg h-13'>                          
+            <div id="play-card-bottom"
+              className='bg-dark-blue rounded-lg grid grid-cols-2 grid-rows-2 gap-x-5 p-7 items-center justify-between'>
+                <span className='font-normal'>Play</span>
+                <img className='justify-self-end' src={dotsSvg}/>
+                <span className='text-3xl font-light'>10hrs</span>
+                <span className='text-pale-blue text-xs justify-self-end'>Last Week - 8hrs</span>
+            </div>
+        </div>
+        <div id="study-card"
+          className='pt-8 bg-study bg-no-repeat bg-right-top bg-light-red-study flex flex-col justify-end rounded-lg h-13'>                          
+            <div id="study-card-bottom"
+              className='bg-dark-blue rounded-lg grid grid-cols-2 grid-rows-2 gap-x-5 p-5 items-center justify-between'>
+                <span className='font-normal'>Study</span>
+                <img className='justify-self-end' src={dotsSvg}/>
+                <span className='text-3xl font-light'>10hrs</span>
+                <span className='text-pale-blue text-xs justify-self-end'>Last Week - 8hrs</span>
+            </div>
+        </div>
+        <div id="exercise-card"
+          className='pt-8 bg-exercise bg-no-repeat bg-right-top bg-lime-green-exercise flex flex-col justify-end rounded-lg h-13'>                          
+            <div id="exercise-card-bottom"
+              className='bg-dark-blue rounded-lg grid grid-cols-2 grid-rows-2 gap-x-5 p-5 items-center justify-between'>
+                <span className='font-normal'>Exercise</span>
+                <img className='justify-self-end' src={dotsSvg}/>
+                <span className='text-3xl font-light'>10hrs</span>
+                <span className='text-pale-blue text-xs justify-self-end'>Last Week - 8hrs</span>
+            </div>
+        </div>
+        <div id="social-card"
+          className='pt-8 bg-social bg-no-repeat bg-right-top bg-violet-social flex flex-col justify-end rounded-lg h-13'>                          
+            <div id="social-card-bottom"
+              className='bg-dark-blue rounded-lg grid grid-cols-2 grid-rows-2 gap-x-5 p-5 items-center justify-between'>
+                <span className='font-normal'>Social</span>
+                <img className='justify-self-end' src={dotsSvg}/>
+                <span className='text-3xl font-light'>10hrs</span>
+                <span className='text-pale-blue text-xs justify-self-end'>Last Week - 8hrs</span>
+            </div>
+        </div>
+        <div id="self-care-card"
+          className='pt-8 bg-self-care bg-no-repeat bg-right-top bg-soft-orange-self-care flex flex-col justify-end rounded-lg h-13'>                          
+            <div id="self-care-card-bottom"
+              className='bg-dark-blue rounded-lg grid grid-cols-2 grid-rows-2 gap-x-5 p-5 items-center justify-between'>
+                <span className='font-normal'>Self Care</span>
+                <img className='justify-self-end' src={dotsSvg}/>
+                <span className='text-3xl font-light'>10hrs</span>
+                <span className='text-pale-blue text-xs justify-self-end'>Last Week - 8hrs</span>
+            </div>
+        </div>
+        
+      </main>
     </>
   )
 }
-
-export default App
